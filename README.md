f7f6fa59d1e2585a667f62a1088cb0f53683e4b0
Samira Ouaaz <souaaz@yahoo.com>
2021-12-11 15:16:39 -0500

    sample node app

---
+++ README.md
@@ -0,0 +1,8 @@
+node app/app.js
+
+THEN:
+curl http://localhost:6000
+
+SHOULD GET:
+Hello World
+
---
+++ app/app.js
@@ -0,0 +1,19 @@
+// Load the express module.
+var http = require('http')
+var PORT = 6000
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+
+
+}).listen(PORT);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:'+PORT+'/');
+
